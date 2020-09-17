12906e83bf0ae2f0189e0566804e5b80542a25b7
Shreyes Srivastava <shreyessrivastava@gmail.com>
2020-09-17 12:17:24 +0530

    change xpath for the user though

--- automation.py
+++ automation.py
@@ -1,3 +1,23 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-print("Hello")
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://web.whatsapp.com/')
+
+name = input('Enter the name of user or group : ')
+msg = input('Enter your message : ')
+count = int(input('Enter the count : '))
+
+input('Enter anything after scanning QR code')
+
+user = driver.find_element_by_xpath('//span[@title="{}"]'.format(name))
+user.click()
+
+msg_box = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]')
+
+for i in range(count):
+    msg_box.send_keys(msg)
+    button = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]/button/span')
+    button.click()
+    time.sleep(5)
